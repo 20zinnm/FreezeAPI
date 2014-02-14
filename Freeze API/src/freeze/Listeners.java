c492e0195c383f77c7338eeb819d1c2b62b379df
20zinnm <meyerzinn@gmail.com>
2014-02-13 23:22:00 -0600

    Anti TP
    I added a nifty listener to keep the frozen player from teleporting.

--- Freeze API/src/freeze/Listeners.java
+++ Freeze API/src/freeze/Listeners.java
@@ -8,14 +8,16 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-
-import freeze.Freeze;
+import org.bukkit.event.player.PlayerTeleportEvent;
 
 public class Listeners implements Listener {
 
 	@SuppressWarnings("static-access")
 	@EventHandler
 	public void onPlayerMove(PlayerMoveEvent e) {
+		if (!Arrays.asList(Freeze.frozen).contains(e.getPlayer())) {
+			return;
+		}
 		if (Arrays.asList(Freeze.frozen).contains(e.getPlayer())) {
 			e.setCancelled(true);
 			e.getPlayer().sendMessage(ChatColor.DARK_AQUA + "You can't move! Your" + ChatColor.WHITE.ITALIC + "frozen!");
@@ -27,6 +29,16 @@ public class Listeners implements Listener {
 		if (Arrays.asList(Freeze.frozen).contains(e.getPlayer())) {
 			Arrays.asList(Freeze.frozen).remove(e.getPlayer());
 			Bukkit.getServer().getPluginManager().callEvent(new PlayerUnfrozenEvent(e.getPlayer()));
+		}
+	}
+	@SuppressWarnings("static-access")
+	public void onPlayerTeleport(PlayerTeleportEvent e) {
+		if (!Arrays.asList(Freeze.frozen).contains(e.getPlayer())) {
+			return;
+		}
+		if (Arrays.asList(Freeze.frozen).contains(e.getPlayer())) {
+			e.setCancelled(true);
+			e.getPlayer().sendMessage(ChatColor.DARK_AQUA + "You can't teleport! Your" + ChatColor.WHITE.ITALIC + "frozen!");
 		}
 	}
 }
